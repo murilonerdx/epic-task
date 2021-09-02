a4c38b1b5a2ec15fa7cde26cf7992f1800c57bc7
murilonerdx <mu-silva@outlook.com>
2021-09-01 23:42:10 -0300

    Adicionando Login e redirecionamento

--- src/main/java/com/murilonerdx/epictask/controller/LoginController.java
+++ src/main/java/com/murilonerdx/epictask/controller/LoginController.java
@@ -4,12 +4,15 @@ import com.murilonerdx.epictask.entities.Perfil;
 import com.murilonerdx.epictask.entities.Tarefa;
 import com.murilonerdx.epictask.entities.Usuario;
 import com.murilonerdx.epictask.repository.UsuarioRepository;
+import com.murilonerdx.epictask.services.impl.TarefaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -19,18 +22,23 @@ public class LoginController {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private TarefaServiceImpl tarefaService;
+
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public String login(Usuario usuario){
         return "login";
     }
 
     @RequestMapping(value="/fazerLogin", method = RequestMethod.POST)
-    public String fazerLogin(@Valid Usuario user,  BindingResult result, Model model){
+    public ModelAndView fazerLogin(@Valid Usuario user,  BindingResult result, Model model){
         Usuario buscarUsuario = repository.findByEmail(user.getEmail());
         if(buscarUsuario != null && buscarUsuario.getPassword().equals(user.getPassword())){
-            return "redirect:/tarefas";
+            return new ModelAndView("tarefas").addObject("tarefas", tarefaService.searchPaginetedTarefas(PageRequest.of(0, 5)));
         }
-        return "login";
+        model.addAttribute("errorValid", "Email invalido");
+        model.addAttribute("tarefas", tarefaService.searchPaginetedTarefas(PageRequest.of(0, 5)));
+        return new ModelAndView("login");
     }
 
     @RequestMapping(value="/cadastrar", method = RequestMethod.GET)
